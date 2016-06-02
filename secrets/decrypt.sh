8aeb6052865b58498f24efa0e6a93eee3c23b4b7
Matt Lucas <mattlucas719@gmail.com>
2016-06-02 11:37:32 -0400

    adding some things

--- secrets/decrypt.sh
+++ secrets/decrypt.sh
@@ -8,6 +8,4 @@ cd $ABSPATH   # Change to the dir where this script exists no matter how its cal
 echo "Pulling any new keys and importing them."
 ../pubkeys/import_all_keys.sh
 
-gpg --decrypt -o $INSECURE_FILE $FILE
-gpg --encrypt --always-trust -r gssteam $INSECURE_FILE
-rm $INSECURE_FILE
+gpg --decrypt -o DECRYPTED_$INSECURE_FILE $FILE
